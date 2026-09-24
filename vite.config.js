93ff20074c0430b2135ab9a2c792afb13ff92ff2
SerzOvod <sergey.ovodenko@gmail.com>
2026-09-24 13:37:13 +0300

    chore: configure vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hotel-booking/',
   plugins: [react()],
   test: {
-    environment: 'jsdom', // Simulates a browser environment in Node.js
-    globals: true, // Allows using describe, test, expect without explicit imports
-    setupFiles: './test/setup.js', // (or .js) Global test setup file
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './test/setup.js',
   },
 });
